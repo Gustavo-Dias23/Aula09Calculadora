6e4f348d10536d487ce2beeb1d4ae5e2c9258721
Gustavo Dias <dias.gustavo23@gmail.com>
2020-06-03 11:22:54 -0300

    Início

---
+++ Calculadora.cs
@@ -0,0 +1,15 @@
+namespace Aula09Calculadora
+{
+    public class Calculadora
+    {
+        public float num1 { get; set; }
+        public float num2 { get; set; }
+        public float resultado { get; set; }
+        public string operador { get; set; }
+
+        public void Somar(){
+            resultado = num1 + num2;
+        }
+        public
+    }
+}
